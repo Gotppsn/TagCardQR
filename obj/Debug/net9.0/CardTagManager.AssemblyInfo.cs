c3b4553a43d774a08337d14f0f8d37767c0276e4
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-03-18 17:13:22 +0700

    error

--- obj/Debug/net9.0/CardTagManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/CardTagManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af7ae1dc09debbd1de8fd1fc73defed319d03410")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a81a1d0bc02354f8475c8d8b6ffdb84f702e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
