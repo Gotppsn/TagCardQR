1070a8626ec8476ea83529e8bfa1d3aee67949eb
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-03-24 10:36:30 +0700

    error upload

--- Models/CardDocument.cs
+++ Models/CardDocument.cs
@@ -18,27 +18,27 @@ namespace CardTagManager.Models
         
         [Required]
         [StringLength(100)]
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
         
         [StringLength(50)]
-        public string DocumentType { get; set; }
+        public string DocumentType { get; set; } = string.Empty;
         
-        public string Description { get; set; }
-        
-        [Required]
-        public string FilePath { get; set; }
+        public string Description { get; set; } = string.Empty;
         
         [Required]
-        public string FileName { get; set; }
+        public string FilePath { get; set; } = string.Empty;
+        
+        [Required]
+        public string FileName { get; set; } = string.Empty;
         
         public long FileSize { get; set; }
         
-        public string FileType { get; set; }
+        public string FileType { get; set; } = string.Empty;
         
         public DateTime UploadedAt { get; set; } = DateTime.Now;
         
         [StringLength(100)]
-        public string UploadedBy { get; set; }
+        public string UploadedBy { get; set; } = string.Empty;
         
         [NotMapped]
         public IFormFile DocumentFile { get; set; }
