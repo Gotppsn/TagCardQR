c93059ed8f206e0c1100404778807435184eaf2c
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-03-25 09:42:06 +0700

    test

--- Controllers/IssueReportController.cs
+++ Controllers/IssueReportController.cs
@@ -62,6 +62,7 @@ namespace CardTagManager.Controllers
 
         // POST: api/IssueReport
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<ActionResult<IssueReport>> CreateIssue([FromBody] IssueReport issue)
         {
             try 
--- obj/Debug/net9.0/CardTagManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/CardTagManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33e3a7dc12a60c32f473d5b50dbd5270beb2ff09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd60003b42d68cce2417799d9c8b821669573022")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
