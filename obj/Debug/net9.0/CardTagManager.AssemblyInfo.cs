344d28d0009be2bd481048f9924a0b02477beaa7
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-03-26 11:43:14 +0700

    update

--- obj/Debug/net9.0/CardTagManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/CardTagManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54de6f0ffcf7b61363cfc60b92015f668fde0ff5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c258c0b051a53d09cd714851fbf95a49a481fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardTagManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
