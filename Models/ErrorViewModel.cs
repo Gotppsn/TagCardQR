85561caaf0c5bb11370399e371dd786e684768a8
Got Panupol <126560479+Gotppsn@users.noreply.github.com>
2025-04-08 16:31:15 +0700

    update

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-// Models/ErrorViewModel.cs
-namespace CardTagManager.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
